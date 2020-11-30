9407bb906cb9671e5569b5ca04df5e787945c9d9
unknown <stefanwetzig@yahoo.de>
2020-12-01 00:18:19 +0100

    Initial Commit

---
+++ src/Main/Movement.java
@@ -0,0 +1,29 @@
+package Main;
+
+public class Movement {
+
+	private Planet origin;
+	private Planet target;
+	
+	
+	public Movement(Planet origin, Planet target) {
+		this.origin = origin;
+		this.target = target;
+	}
+	
+	
+	public Planet getOrigin() {
+		return origin;
+	}
+	public void setOrigin(Planet origin) {
+		this.origin = origin;
+	}
+	public Planet getTarget() {
+		return target;
+	}
+	public void setTarget(Planet target) {
+		this.target = target;
+	}
+	
+	
+}
