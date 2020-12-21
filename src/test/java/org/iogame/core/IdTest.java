e409bef39ac0ae85fd67fed3b3f47b9c83a2b5d0
Soren <soer3nreichardt@googlemail.com>
2020-12-21 17:48:58 +0100

    Remove environment logic from Id class to fix map lookup

--- src/test/java/org/iogame/core/IdTest.java
+++ src/test/java/org/iogame/core/IdTest.java
@@ -6,9 +6,10 @@ import org.iogame.model.planet.Planet;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.iogame.StaticSettings.ENV;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class IdTest {
 
@@ -31,7 +32,7 @@ class IdTest {
     @Test
     void testFromString() {
         Id devId = Id.fromString("42");
-        assertEquals(Long.valueOf(42), devId.getValue());
+        assertEquals(Long.valueOf(42), devId.getId());
     }
 
     @Test
@@ -40,8 +41,17 @@ class IdTest {
         Id planetId2 = Id.generateForClass(Planet.class);
         Id fleetId = Id.generateForClass(Fleet.class);
 
-        assertEquals(0L, planetId1.<Long>getValue());
-        assertEquals(1L, planetId2.<Long>getValue());
-        assertEquals(0L, fleetId.<Long>getValue());
+        assertEquals(0L, planetId1.<Long>getId());
+        assertEquals(1L, planetId2.<Long>getId());
+        assertEquals(0L, fleetId.<Long>getId());
+    }
+
+    @Test
+    void shouldBeEqualInMapGet() {
+        Id id1 = new Id(42);
+        Id id2 = new Id(42);
+        Map<Id, String> map = Map.of(id1, "foo");
+
+        assertEquals("foo", map.get(id2));
     }
 }
